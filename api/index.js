121a207c31da8559aa9e32f9f799f291c20da345
amirreza_yar <yar.amirreza@gmail.com>
2025-08-11 22:03:16 +0330

    minor change in index.js

--- api/index.js
+++ api/index.js
@@ -25,16 +25,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 const PORT = process.env.PORT || 3000;
 
 async function start() {
-  await connectDB();
-  app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+    await connectDB();
+    app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+    console.info('app and db started successfuly')
 }
 
 // start only when executed directly (not when imported by /api/index.js)
-if (require.main === module) {
-  start().catch(err => {
+
+start().catch(err => {
     console.error('Failed to start', err);
     process.exit(1);
-  });
-}
+});
 
 module.exports = app;
