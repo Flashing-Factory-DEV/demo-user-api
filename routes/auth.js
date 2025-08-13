135358e4b22400fce605261c98241f6817d2ccc7
amirreza_yar <yar.amirreza@gmail.com>
2025-08-13 16:09:11 +0330

    Cookie options updated

--- routes/auth.js
+++ routes/auth.js
@@ -24,8 +24,10 @@ const { sendVerificationSMS } = require('../utils/sms');
 // Login cookie options
 const cookieOptions = {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production', // HTTPS only in production
-    sameSite: 'strict',
+    secure: false,
+    sameSite: 'lax',
+    path: '/',
+
     maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
 };
 
